98dcbb9ec44bfc19cccb295f35080a20eccbddb9
Anthonchess <anthonchess@gmail.com>
2026-04-20 17:31:24 -0400

    adding favicon of las lomas2

--- dashboard.php
+++ dashboard.php
@@ -74,6 +74,8 @@ $amenityValues = array_values($amenities);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Las Lomas Survey Dashboard</title>
     <link rel="icon" href="/img/favicon.svg" type="image/svg+xml">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="apple-touch-icon" href="/img/logo-fallback.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
